5a905a7d7e8262b987a64f47a498dcc306ba4de2
Vincent Marquet <vincent.marquet1@free.fr>
2014-10-22 19:28:29 +0200

    added Twitter Bootstrap gem
    J'ai suivi le tuto http://railscasts.com/episodes/328-twitter-bootstrap-basics?autoplay=true
    
    Résumé des commandes:
    
    * ajout des gems nécéssaires dans le fichier gemfile
    * bundle install  -> pour installer les nouvelles gems
    * rails g bootstrap:install  -> pour générer les fichiers de base de Twitter Bootstrap

--- server-ror/db/schema.rb
+++ server-ror/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017134558) do
+ActiveRecord::Schema.define(version: 20141017164455) do
 
   create_table "articles", force: true do |t|
     t.string   "name"
