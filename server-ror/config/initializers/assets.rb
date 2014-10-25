cc6d76c43c95aeca67029da92a48a89da000b91e
Vincent Marquet <vincent.marquet1@free.fr>
2014-10-25 14:48:24 +0200

    ajout barre de navigation (la barre tout en haut)
    J'ai voulu faire un truc simple avec Twitter Bootstrap (cf http://getbootstrap.com/2.3.2/components.html#navbar)
    mais j'ai pas réussi, du coup j'ai utilisé un template css (navbar.css)
    
    pour ce faire:
    
    1 j'ai écrit le code HTML de la navbar dans app/views/layouts/application.html.erb
      (rappel: c'est le fichier HTML qui sert de base pour toutes les pages du site)
    
    2 pour ajouter le fichier navbar.css au projet,
      cf la doc: http://guides.rubyonrails.org/asset_pipeline.html#how-to-use-the-asset-pipeline
    
    Remarque (cf app/views/layouts/application.html.erb):
    pour faire un lien HTML vers une page en rails, j'ai utilisé le code Ruby suivant (à insérer dans le HTML):
    <%= link_to "Articles", :controller => :articles, :action => :index %>
    
    Explication: c'est un appel à la fonction link_to, avec 3 paramètres:
    1. "Articles" -> le texte du lien qui apparaîtra sur la page
    2. :articles -> le controlleur qui génère la page visée par le lien
    3. :index -> la méthode du controlleur indiqué en argument 2
    
    Au final, cela donnera:  <a href="/articles">Articles</a>
    L'avantage d'avoir utilisé le code Ruby pour génerer le lien plutôt que d'avoir écrit directement le HTML
    est que si un jour la page vers laquelle on pointe n'est plus valide (on supprime le controlleur
    ou sa méthode index), Rails nous mettra tout de suite une erreur, alors qu'en ayant codé le lien en HTML
    directement, si ensuite la page pointée n'est plus valide, Rails ne dira rien et on aura un lien mort

--- server-ror/config/initializers/assets.rb
+++ server-ror/config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+
+# le fichier css pour la barre de navigation tout en haut
+Rails.application.config.assets.precompile += %w( navbar.css )
