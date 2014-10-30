0978eca55cdd795d1cc31ce3c8c7876fc7017fbe
Vincent Marquet <vincent.marquet1@free.fr>
2014-10-30 14:30:03 +0100

    resolve #1 (cf http://stackoverflow.com/questions/6541209/)

--- server-ror/app/models/article.rb
+++ server-ror/app/models/article.rb
@@ -11,6 +11,9 @@ class Article < ActiveRecord::Base
 	# we check that some fields aren't nil
 	validates_presence_of :name, :price
 
+	# we convert the input (see after in this file for the explanation about this)
+	attr_localized :price
+
 	# we check that the price is correct
 	validates :price, :format => { :with => /\A\d+(?:\.\d{0,2})?\z/ },
 	          :numericality => {:greater_than => 0.0, :less_than => 999999.99}
@@ -38,4 +41,29 @@ class Article < ActiveRecord::Base
 	end
 	
 end
+
+
+##########
+# we want to redefine the way ActiveRecord should handle the price
+# (because the way people will enter the price in the form depends on their country)
+# to do this, we add a new method to String class and to ActiveRecord class
+class String
+	def to_delocalized_decimal
+		delimiter = I18n::t('number.format.delimiter')
+		separator = I18n::t('number.format.separator')
+		self.gsub(/[#{delimiter}#{separator}]/, delimiter => '', separator => '.')
+	end
+end
+
+class ActiveRecord::Base
+	def self.attr_localized(*fields)
+		fields.each do |field|
+			define_method("#{field}=") do |value|
+				self[field] = value.is_a?(String) ? value.to_delocalized_decimal : value
+			end
+		end
+	end
+end
+# cf http://stackoverflow.com/questions/6541209/
+##########
 
