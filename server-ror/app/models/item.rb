93302ec60a63e65a0d2577fc8974388362f6d844
Vincent Marquet <vincent.marquet1@free.fr>
2014-11-05 21:10:01 +0100

    price of an item is automatically copied from the article price

--- server-ror/app/models/item.rb
+++ server-ror/app/models/item.rb
@@ -8,11 +8,14 @@ class Item < ActiveRecord::Base
 
 	belongs_to :article
 	validates_presence_of :article
+	validates_presence_of :article_id
 
-	# TODO:
-	# - il faudrait récupérer le prix de l'article et le copier dans la colonne prix
-	#   de l'item avant de l'insérer dans la base
-	# - et vérifier avant insertion qu'il n'est pas nul
-	#   (dans le modèle + une migrations)
+	# we use ActiveRecord callbacks to copy the price of the article
+	# (we must save a copy of the price, in case the bartender changes the prices)
+	before_save do
+		|item| item.price = Article.find(item.article_id).price
+	end
+	# cf http://stackoverflow.com/questions/1187138
+	# et http://api.rubyonrails.org/classes/ActiveRecord/Callbacks.html
 
 end
