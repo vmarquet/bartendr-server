44109acdb597dd30ffc3101c26388212db624eff
Vincent Marquet <vincent.marquet1@free.fr>
2014-11-06 15:21:41 +0100

    add controller for the pages not linked to a resource (like the homepage)

--- server-ror/config/routes.rb
+++ server-ror/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   resources :orders
 
   resources :articles
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'articles#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
