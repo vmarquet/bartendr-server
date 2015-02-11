9d8e07af84c129639b53c9a47d4a209411e0ea0b
vmarquet <vincent.marquet1@free.fr>
2015-02-12 00:07:25 +0100

    add /users page, to display all staff info

--- server-ror/app/controllers/users_controller.rb
+++ server-ror/app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+  	@users = User.where(["approved = ?", true])
+  end
+
   def activate_accounts
     # @users = User.find_all_by_approved(false)
     # the previous line should work, but method 'find_all_by_approved' is not found
--- server-ror/config/routes.rb
+++ server-ror/config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   patch '/orders/:id/ispaid', to: 'orders#ispaid'
   patch '/orders/:id/isserved', to: 'orders#isserved'
 
+  get '/users', to: 'users#index'
   devise_for :users, path: "auth", path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   get '/activate_accounts', to: 'users#activate_accounts'
 
