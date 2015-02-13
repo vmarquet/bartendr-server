ee923447440a20ae4a8ae9a5c05483f6153594b2
vmarquet <vincent.marquet1@free.fr>
2015-02-13 19:46:10 +0100

    add test to check that admin and bartender have proper rights
    le test échoue (comme pas mal d'autres), visiblement le code
    after_create dans `user.rb` qui donne les droits aux premiers et second
    à s'enregistrer n'est pas exécuté.
    
    A élucider parce que c'est un point un peu critique et ce serait bien
    d'avoir des tests là-dessus.

--- server-ror/test/controllers/users_controller_test.rb
+++ server-ror/test/controllers/users_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+class UserTest < ActiveSupport::TestCase
+  test "first user to register should have admin rights" do
+    assert (User.find(1).has_role? :admin), "Admin doesn't have admin rights"
+  end
+
+  test "second user to register should have bartender rights" do
+    assert (User.find(2).has_role? :bartender), "Bartender doesn't have bartender rights"
+  end
 end
