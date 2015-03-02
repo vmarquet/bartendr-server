568fc8f9eea457a7a913d1ad0ff33f357635d907
vmarquet <vincent.marquet1@free.fr>
2015-03-02 15:48:38 +0100

    add page with all staff info, including each person's permissions go at /users or click 'staff' in the navigation bar

--- server-ror/app/controllers/registrations_controller.rb
+++ server-ror/app/controllers/registrations_controller.rb
@@ -1,10 +1,18 @@
 class RegistrationsController < Devise::RegistrationsController
-  # cf http://www.jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html
-  # we must override some devise methods
-  # in order to allow our custom parameters such as first_name or last_name
+  protected
+
+  # we override the redirection path after a user edit his profile
+  def after_update_path_for resource
+    users_path
+  end
+
 
   private
  
+  # cf http://www.jacopretorius.net/2014/03/adding-custom-fields-to-your-devise-user-model-in-rails-4.html
+  # we must override some devise methods
+  # in order to allow our custom parameters such as first_name or last_name:
+
   def sign_up_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
